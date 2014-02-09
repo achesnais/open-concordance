d937de6c2cb7114508e3f553701a76b09ac8e3bc
Antoine Chesnais <achesnais@gmail.com>
2014-02-09 16:40:13 +0100

    Normalised beginnings of source files, adding license information and simple file description

--- corpus.py
+++ corpus.py
@@ -1,152 +1,172 @@
-import os, time, threading
+#!/usr/bin/python3
+
+"""
+    Open Concordance by Antoine Chesnais. A simple corpus processing tool.
+    Copyright (C) 2014  Antoine Chesnais
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os, time, threading # Time is used for testing purposes.
 
 class Corpus:
-	def __init__(self, path=None):
-		self.texts = {}
-		if path and os.path.exists(path):
-			if os.path.isdir(path):
-				self.load_folder(path)
-			else:
-				self.load_text_file(path)
+    def __init__(self, path=None):
+        self.texts = {}
+        if path and os.path.exists(path):
+            if os.path.isdir(path):
+                self.load_folder(path)
+            else:
+                self.load_text_file(path)
 
-	def load_text_file(self, path):
-		if not os.path.exists(path):
-			raise IOError
-		with open(path) as f:
-			self.texts[path] = tokenize(f.read())
+    def load_text_file(self, path):
+        if not os.path.exists(path):
+            raise IOError
+        with open(path) as f:
+            self.texts[path] = tokenize(f.read())
 
-	def load_folder(self, path):
-		print('Loading texts.')
-		if not os.path.exists(path):
-			raise IOError
-		filelist = os.listdir(path)
-		pathlist = []
-		for f in filelist:
-			pathlist.append(path+'/'+f)
-		for p in pathlist:
-			with open(p) as f:
-				self.texts[p] = tokenize(f.read())
-		print('Loaded {} texts.'.format(len(pathlist)))
+    def load_folder(self, path):
+        print('Loading texts.')
+        if not os.path.exists(path):
+            raise IOError
+        filelist = os.listdir(path)
+        pathlist = []
+        for f in filelist:
+            pathlist.append(path+'/'+f)
+        for p in pathlist:
+            with open(p) as f:
+                self.texts[p] = tokenize(f.read())
+        print('Loaded {} texts.'.format(len(pathlist)))
 
-	def get_token_size(self):
-		size = 0
-		for t in self.texts:
-			size += len(self.texts[t])
-		return size
+    def get_token_size(self):
+        size = 0
+        for t in self.texts:
+            size += len(self.texts[t])
+        return size
 
-	def concordance(self, word, left=10, right=10, case=False, output='std'):
-		"""Aims to be your typical concordancer with KWIC.
-		
-		TODO: left_text and right_text made be made into lists to allow for easier 
-		KWIC sorting.
-		Also, max_right and max_left variables could be added to permit to oversee
-		punctuation in span count, though apparently span count is done in characters,
-		not words or tokens (as seen in AntConc)
-		Obviously, implement search of multiple groups, and when that's functional, 
-		regular expression search."""
-		
-		if case:
-			match_func = lambda w, t: w == t
-		else:
-			word = word.lower()
-			match_func = lambda w, t: w == t.lower() # word is already lowered, see line above
-		results = []
-		hits = 0
-		for t in sorted(self.texts):
-			for index, token in enumerate(self.texts[t]):
-				if match_func(word, token):
-					left_text = ""
-					right_text = ""
-					for i in range(max(0, index-left), index, 1):
-						if self.texts[t][i].isalnum():
-							left_text += ' ' + self.texts[t][i]
-						elif self.texts[t][i] in ('.', '?', '!', ',', ';', ':', ')'):
-							left_text += self.texts[t][i]
-						else:
-							left_text += ' ' + self.texts[t][i]
-						
-					for i in range(index + 1, min(index+right+1, len(self.texts[t])), 1):
-						if self.texts[t][i].isalnum():
-							right_text += ' ' + self.texts[t][i]
-						elif self.texts[t][i] in ('.', '?', '!', ',', ';', ':', ')'):
-							right_text += self.texts[t][i]
-						else:
-							right_text += " " + self.texts[t][i]
-					results.append((token, left_text.strip(), right_text.strip(), t))
-					hits += 1
-		if output == 'txt':
-			l_width = r_width = p_width = 0
-			w_width = len(word)
-			for r in results:
-				l_width = max(l_width, len(r[1]))
-				r_width = max(r_width, len(r[2]))
-				p_width = max(p_width, len(r[3]))
-				
-			with open('{}_concordance_results.txt'.format(word), 'w') as f:
-				f.write("There are {} hits.\n\n".format(hits))
-				for r in results:
-					f.write("{0:>{lw}} <{1:^{ww}}> {2:<{rw}}    in file {3:<{pw}}.\n\n".format(r[1], r[0], r[2], r[3], lw=l_width, rw=r_width, ww=w_width, pw=p_width))
-		else:
-			return hits, results
-	
-	def quick_concordance(self, word, case=False):
-		if case:
-			match_func = lambda w, t: w == t
-		else:
-			match_func = lambda w, t: w == t.lower() # word is already lowered, see two lines below
-		hits = 0
-		word = word.lower()
-		for t in self.texts:
-			for token in self.texts[t]:
-				if match_func(word, token):
-					hits += 1
-		return hits
-	
-	def word_list(self, case=False, punc=False, num=False):
-		for t in self.texts:
-			pass
-	
-	def get_data(self):
-		min_length = None
-		max_length = None
-		length_sum = 0
-		for t in self.texts:
-			length = len(self.texts[t])
-			if min_length:
-				min_length = min(length, min_length)
-			else:
-				min_length = length
-			if max_length:
-				max_length = max(length, max_length)
-			else:
-				max_length = length
-			length_sum += length
-		print("Smallest text is {} tokens long.".format(min_length))
-		print("Longest text is {} tokens long.".format(max_length))
-		print("Average length is {:2} tokens".format(length_sum/len(self.texts)))
+    def concordance(self, word, left=10, right=10, case=False, output='std'):
+        """Aims to be your typical concordancer with KWIC.
+        
+        TODO: left_text and right_text made be made into lists to allow for easier 
+        KWIC sorting.
+        Also, max_right and max_left variables could be added to permit to oversee
+        punctuation in span count, though apparently span count is done in characters,
+        not words or tokens (as seen in AntConc)
+        Obviously, implement search of multiple groups, and when that's functional, 
+        regular expression search."""
+        
+        if case:
+            match_func = lambda w, t: w == t
+        else:
+            word = word.lower()
+            match_func = lambda w, t: w == t.lower() # word is already lowered, see line above
+        results = []
+        hits = 0
+        for t in sorted(self.texts):
+            for index, token in enumerate(self.texts[t]):
+                if match_func(word, token):
+                    left_text = ""
+                    right_text = ""
+                    for i in range(max(0, index-left), index, 1):
+                        if self.texts[t][i].isalnum():
+                            left_text += ' ' + self.texts[t][i]
+                        elif self.texts[t][i] in ('.', '?', '!', ',', ';', ':', ')'):
+                            left_text += self.texts[t][i]
+                        else:
+                            left_text += ' ' + self.texts[t][i]
+                        
+                    for i in range(index + 1, min(index+right+1, len(self.texts[t])), 1):
+                        if self.texts[t][i].isalnum():
+                            right_text += ' ' + self.texts[t][i]
+                        elif self.texts[t][i] in ('.', '?', '!', ',', ';', ':', ')'):
+                            right_text += self.texts[t][i]
+                        else:
+                            right_text += " " + self.texts[t][i]
+                    results.append((token, left_text.strip(), right_text.strip(), t))
+                    hits += 1
+        if output == 'txt':
+            l_width = r_width = p_width = 0
+            w_width = len(word)
+            for r in results:
+                l_width = max(l_width, len(r[1]))
+                r_width = max(r_width, len(r[2]))
+                p_width = max(p_width, len(r[3]))
+                
+            with open('{}_concordance_results.txt'.format(word), 'w') as f:
+                f.write("There are {} hits.\n\n".format(hits))
+                for r in results:
+                    f.write("{0:>{lw}} <{1:^{ww}}> {2:<{rw}}    in file {3:<{pw}}.\n\n".format(r[1], r[0], r[2], r[3], lw=l_width, rw=r_width, ww=w_width, pw=p_width))
+        else:
+            return hits, results
+    
+    def quick_concordance(self, word, case=False):
+        if case:
+            match_func = lambda w, t: w == t
+        else:
+            match_func = lambda w, t: w == t.lower() # word is already lowered, see two lines below
+        hits = 0
+        word = word.lower()
+        for t in self.texts:
+            for token in self.texts[t]:
+                if match_func(word, token):
+                    hits += 1
+        return hits
+    
+    def word_list(self, case=False, punc=False, num=False):
+        for t in self.texts:
+            pass
+    
+    def get_data(self):
+        min_length = None
+        max_length = None
+        length_sum = 0
+        for t in self.texts:
+            length = len(self.texts[t])
+            if min_length:
+                min_length = min(length, min_length)
+            else:
+                min_length = length
+            if max_length:
+                max_length = max(length, max_length)
+            else:
+                max_length = length
+            length_sum += length
+        print("Smallest text is {} tokens long.".format(min_length))
+        print("Longest text is {} tokens long.".format(max_length))
+        print("Average length is {:2} tokens".format(length_sum/len(self.texts)))
 
 def tokenize(string):
-	tokens = []
-	word = ''
-	for char in string:
-		if char.isalnum():
-			word += char
-		elif char not in (' ', '\n'):
-			if word:
-				tokens.append(word)
-				word = ''
-			tokens.append(char)
-		else:
-			if word:
-				tokens.append(word)
-				word = ''
-	return tokens
-	
+    tokens = []
+    word = ''
+    for char in string:
+        if char.isalnum():
+            word += char
+        elif char not in (' ', '\n'):
+            if word:
+                tokens.append(word)
+                word = ''
+            tokens.append(char)
+        else:
+            if word:
+                tokens.append(word)
+                word = ''
+    return tokens
+    
 
 
 def main():
-	c = Corpus('txts')
-	c.get_data()
+    c = Corpus('txts')
+    c.get_data()
 
 if __name__ == '__main__':
-	main()
+    main()
--- filetools.py
+++ filetools.py
@@ -1,121 +1,143 @@
+#!/usr/bin/python3
+
+"""
+    Tools to manipulate corpus files. Written for my current linguistics
+    research project.
+    
+    Copyright (C) 2014  Antoine Chesnais
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 
 CORPUS_PATH = './txts'
 LIMIT = 'volume'
 
 def comment_out_text_info(limit_word=LIMIT):
-	"""Comments out info which should not be taken into account
-	by AntConc."""
-	path_list = generate_path_list(CORPUS_PATH)
-	for p in path_list:
-		buff = []
-		with open(p) as f:
-			out = True
-			for line in f:
-				if not line.startswith('#'):
-					if line.lower().startswith(limit_word) and out:
-						out = False
-						buff.append('#' + line)
-					elif out:
-						buff.append('#' + line)
-					else:
-						buff.append(line)
-		with open(p, 'w') as f:
-			for i in buff:
-				f.write(i)
-				
+    """Comments out info which should not be taken into account
+    by AntConc."""
+    path_list = generate_path_list(CORPUS_PATH)
+    for p in path_list:
+        buff = []
+        with open(p) as f:
+            out = True
+            for line in f:
+                if not line.startswith('#'):
+                    if line.lower().startswith(limit_word) and out:
+                        out = False
+                        buff.append('#' + line)
+                    elif out:
+                        buff.append('#' + line)
+                    else:
+                        buff.append(line)
+        with open(p, 'w') as f:
+            for i in buff:
+                f.write(i)
+                
 def remove_sharp():
-	path_list = generate_path_list(CORPUS_PATH)
-	for p in path_list:
-		buff = []
-		with open(p) as f:
-			for line in f:
-				if not line.startswith('#'):
-					buff.append(line)
-		with open(p, 'w') as f:
-			for i in buff:
-				f.write(i)
+    path_list = generate_path_list(CORPUS_PATH)
+    for p in path_list:
+        buff = []
+        with open(p) as f:
+            for line in f:
+                if not line.startswith('#'):
+                    buff.append(line)
+        with open(p, 'w') as f:
+            for i in buff:
+                f.write(i)
 
 def generate_path_list(corpus_path):
-	"""Returns the paths of files of a corpus
-	
-	Returns the files in the form of a list of
-	strings containing their paths.
-	PLEASE NOTE THIS WILL ONLY WORK IN UNIX!"""
-	
-	path_list = []
-	file_list = os.listdir(corpus_path)
-	for f in file_list:
-			path_list.append(os.path.join(corpus_path,f))
-	return path_list
+    """Returns the paths of files of a corpus
+    
+    Returns the files in the form of a list of
+    strings containing their paths.
+    PLEASE NOTE THIS WILL ONLY WORK IN UNIX!"""
+    
+    path_list = []
+    file_list = os.listdir(corpus_path)
+    for f in file_list:
+            path_list.append(os.path.join(corpus_path,f))
+    return path_list
 
 def check_double_titles(path_list):
-	for p in path_list:
-		with open(p) as f:
-			l = f.readline()
-			if l in title_list:
-				print(l)
-			else:
-				title_list.append(l)
-	for i in title_list:
-		print(i)
+    for p in path_list:
+        with open(p) as f:
+            l = f.readline()
+            if l in title_list:
+                print(l)
+            else:
+                title_list.append(l)
+    for i in title_list:
+        print(i)
 
 def remove_figures():
-	for p in generate_path_list('processed_txts'):
-		running = True
-		done = False
-		while not done:
-			done = True
-			buff = []
-			previous_line = 'none'
-			out = False
-			changed = False
-			with open(p) as f:
-				for line in f:
-					if line.lower().startswith(('figure', 'table', 'image')) or out:
-						out = True
-						os.system('clear')
-						print('Removal tool')
-						print('File: {}'.format(p))
-						print()
-						print(previous_line)
-						print(">>> " + line)
-						print()
-						cmd = input("Delete?(n/Y/q/r)").lower()
-						if cmd == 'q':
-							running = False
-							changed = False
-							done = True
-							break
-						elif cmd == 'r':
-							changed = False
-							done = False
-							break
-						elif cmd == 'n':
-							buff.append(line)
-							out = False
-							if line != '\n':
-								previous_line = line
-						else:
-							buff.append('')
-							changed = True
-							
-					else:
-						buff.append(line)
-						if line != '\n':
-							previous_line = line
-			if changed:
-				if input('Save file?(Y/n)') != 'n':
-					with open(p, 'w') as f:
-						for i in buff:
-							f.write(i)
-		if not running:
-			break
+    for p in generate_path_list('processed_txts'):
+        running = True
+        done = False
+        while not done:
+            done = True
+            buff = []
+            previous_line = 'none'
+            out = False
+            changed = False
+            with open(p) as f:
+                for line in f:
+                    if line.lower().startswith(('figure', 'table', 'image')) or out:
+                        out = True
+                        os.system('clear')
+                        print('Removal tool')
+                        print('File: {}'.format(p))
+                        print()
+                        print(previous_line)
+                        print(">>> " + line)
+                        print()
+                        cmd = input("Delete?(n/Y/q/r)").lower()
+                        if cmd == 'q':
+                            running = False
+                            changed = False
+                            done = True
+                            break
+                        elif cmd == 'r':
+                            changed = False
+                            done = False
+                            break
+                        elif cmd == 'n':
+                            buff.append(line)
+                            out = False
+                            if line != '\n':
+                                previous_line = line
+                        else:
+                            buff.append('')
+                            changed = True
+                            
+                    else:
+                        buff.append(line)
+                        if line != '\n':
+                            previous_line = line
+            if changed:
+                if input('Save file?(Y/n)') != 'n':
+                    with open(p, 'w') as f:
+                        for i in buff:
+                            f.write(i)
+        if not running:
+            break
 
 def main():
-	remove_figures()
+    remove_figures()
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
